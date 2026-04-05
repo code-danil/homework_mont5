51f0ad65b44c7a2177fa9fada7323109c11ffd78
code-danil <quantumfoge@gmail.com>
2026-04-05 16:41:48 +0600

    month5

--- product/views.py
+++ product/views.py
@@ -1,52 +1,97 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Category, Products, Review
 from .serializers import CategorySerializer, ProductsSerializer, ReviewSerializer
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def category_list_view(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
-
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def category_detail_view(request, id):
     category = Category.objects.get(id=id)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+    elif request.method == 'DELETE':
+        category.delete()
+        return Response({'message': 'Удалено!'})
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def product_list_view(request):
-    Product = Products.objects.all()
-    serializer = ProductsSerializer(Product, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        serializer = ProductsSerializer(Products.objects.all(), many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail_view(request, id):
-    Product = Products.objects.get(id=id)
-    serializer = ProductsSerializer(Product)
-    return Response(serializer.data)
+    product = get_object_or_404(Products, id=id)
+    if request.method == 'GET':
+        serializer = ProductsSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductsSerializer(product, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response({'message': 'Удалено!'})
 
 
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def review_list_view(request):
-    reviews = Review.objects.all()
-    serializer = ReviewSerializer(reviews, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        reviews = Review.objects.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ReviewSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
-
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def review_detail_view(request, id):
-    review = Review.objects.get(id=id)
-    serializer = ReviewSerializer(review)
-    return Response(serializer.data)
-
+    review = get_object_or_404(Review, id=id)
+    if request.method == 'GET':
+        serializer = ReviewSerializer(review)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ReviewSerializer(review, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response({'message': 'Удалено!'})
 
 @api_view(['GET'])
 def product_reviews_list(request):
